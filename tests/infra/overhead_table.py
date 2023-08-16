b700c67c6cb7141053cfbbad0d0c8d421ab74605
Heidi Howard <1835251+heidihoward@users.noreply.github.com>
2023-08-16 12:10:27 +0000

    fix file naming

--- tests/infra/overhead_table.py
+++ tests/infra/overhead_table.py
@@ -4,6 +4,7 @@ import datetime
 import json
 
 timenow = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# timenow = "2023-08-09_17-37-59"
 
 throughputs = []
 apps = [["--package", "samples/apps/basic/libbasic","--client-def","6,write,1000000,primary"],
@@ -12,13 +13,13 @@ apps = [["--package", "samples/apps/basic/libbasic","--client-def","6,write,1000
        ["--js-app-bundle", "../samples/apps/basic/js/", "--client-def", "50,read,100000,any"],
 ]
 
-for app_conf in apps:
+for i, app_conf in enumerate(apps):
 
-    basicperf_cmd = ["python3", "/home/azureuser/heidi/CCF/tests/infra/basicperf.py", "-b", ".", "-c", "./submit", "--host-log-level", "info", "--enclave-log-level", "info", "--worker-threads", "10", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/actions.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/validate.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/resolve.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/apply.js", "--label", "pi_basic_mt_sgx_cft^", "--client-timeout-s", "200", "--snapshot-tx-interval","20000",  "-e", "release", "-t", "sgx", "--workspace", f"/home/azureuser/heidi/CCF/build-sgx/ws-{timenow}-tab", "-n", "ssh://172.23.0.13", "-n", "ssh://172.23.0.9","-n", "ssh://172.23.0.10", "-n", "ssh://172.23.0.11","-n", "ssh://172.23.0.12"] + app_conf
+    basicperf_cmd = ["python3", "/home/azureuser/heidi/CCF/tests/infra/basicperf.py", "-b", ".", "-c", "./submit", "--host-log-level", "info", "--enclave-log-level", "info", "--worker-threads", "10", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/actions.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/validate.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/resolve.js", "--constitution", "/home/azureuser/heidi/CCF/samples/constitutions/default/apply.js", "--label", "pi_basic_mt_sgx_cft^", "--client-timeout-s", "200", "--snapshot-tx-interval","20000",  "-e", "release", "-t", "sgx", "--workspace", f"/home/azureuser/heidi/CCF/build-sgx/ws-{timenow}-tab-i{i}", "-n", "ssh://172.23.0.13", "-n", "ssh://172.23.0.9","-n", "ssh://172.23.0.10", "-n", "ssh://172.23.0.11","-n", "ssh://172.23.0.12"] + app_conf
     
     subprocess.run(basicperf_cmd)
 
-    stats = json.load(open(f"/home/azureuser/heidi/CCF/build-sgx/ws-{timenow}-tab/pi_basic_mt_sgx_cft^_common/statistics.json"))
+    stats = json.load(open(f"/home/azureuser/heidi/CCF/build-sgx/ws-{timenow}-tab-i{i}/pi_basic_mt_sgx_cft^_common/statistics.json"))
     throughputs.append((app_conf, stats["all_clients_active_average_throughput_tx/s"]))
 
 print("throughputs", throughputs)
